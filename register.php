ba0d2003a8d782fc4e9ca19dfd57ab7e83d71462
gavinitejaswini1234 <gavinitejaswini4@gmail.com>
2025-07-13 20:14:22 +0530

    Create register.php

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include 'db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = trim($_POST['username']);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $role = 'editor'; // default
+
+    if (strlen($username) < 4) {
+        echo "Username too short";
+        exit;
+    }
+
+    $stmt = $pdo->prepare("INSERT INTO users (username, password, role) VALUES (?, ?, ?)");
+    $stmt->execute([$username, $password, $role]);
+
+    echo "Registration successful. <a href='login.php'>Login</a>";
+}
+?>
+<form method="post">
+    <input type="text" name="username" required placeholder="Username"><br>
+    <input type="password" name="password" required placeholder="Password"><br>
+    <input type="submit" value="Register">
+</form>
